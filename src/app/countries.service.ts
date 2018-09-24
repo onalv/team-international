8c3bf37c925d4b532d21ba323c99072eb8c0557c
onalv <omarnavaalvarez@gmail.com>
2018-09-23 21:29:33 -0500

    Modified Countries Service

--- src/app/countries.service.ts
+++ src/app/countries.service.ts
@@ -1,9 +1,48 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import {Observable, of} from 'rxjs';
+import {catchError, map, tap} from 'rxjs/operators';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountriesService {
 
-  constructor() { }
+  private countriesUrl = 'https://restcountries.eu/rest/v2/all';
+
+  constructor(private http: HttpClient) { }
+
+  /** GET countries from the server */
+  getCountries (): Observable<any[]> {
+    return this.http.get<any[]>(this.countriesUrl)
+      .pipe(
+        tap(countries => console.log(countries)),
+        catchError(this.handleError('getHeroes', []))
+      );
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      console.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
 }
