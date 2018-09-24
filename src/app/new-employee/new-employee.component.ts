54c0f202f7464b3c87ce32c52e45f1c945d6b231
onalv <omarnavaalvarez@gmail.com>
2018-09-23 21:10:53 -0500

    added add-employee-component

--- src/app/new-employee/new-employee.component.ts
+++ src/app/new-employee/new-employee.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {Store} from '@ngrx/store';
+import {Router} from '@angular/router';
+import {AppState} from '../app.state';
+import {CountriesService} from '../countries.service';
+
+import * as EmployeeActions from '../actions/employee.actions';
 
 @Component({
   selector: 'app-new-employee',
@@ -7,9 +13,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewEmployeeComponent implements OnInit {
 
-  constructor() { }
+  countries: any[];
+
+  constructor(
+    private store: Store<AppState>,
+    private router: Router,
+    private countryService: CountriesService
+  ) {}
+
+  addEmployee(
+    name,
+    userName,
+    dateOfBirth,
+    hireDate,
+    country,
+    status,
+    area,
+    jobTitle,
+    tipRate,
+    ) {
+      this.store.dispatch(new EmployeeActions.AddEmployee({
+        name,
+        userName,
+        dateOfBirth,
+        hireDate,
+        country,
+        status,
+        area,
+        jobTitle,
+        tipRate,
+      }));
+
+    this.router.navigate(['/']);
+  }
+
+  showMe() {
+    console.log(this.countries);
+  }
 
   ngOnInit() {
+    this.countryService.getCountries()
+      .subscribe(countries => {
+        this.countries = countries.map(country => country['name']);
+      });
   }
 
 }
