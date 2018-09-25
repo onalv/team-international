786a25fd2036ab70fd4869b37429b84464cecb76
onalv <omarnavaalvarez@gmail.com>
2018-09-24 23:15:20 -0500

    Started view-employee component edition

--- src/app/view-employee/view-employee.component.ts
+++ src/app/view-employee/view-employee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-view-employee',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewEmployeeComponent implements OnInit {
 
-  constructor() { }
+  area = 'services';
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  gotoHomepage() {
+    this.router.navigate(['/']);
   }
 
 }
